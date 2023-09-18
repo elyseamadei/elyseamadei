fe1cc0c9692704dfb70dc9c64b30b2ef74a58f12
Ultrabug <ultrabug@ultrabug.net>
2023-09-18 17:47:54 +0200

    add bracelets magic trilogy generator

---
+++ hooks.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import glob
+import exifread
+
+def on_page_markdown(markdown, page, config, files):
+
+    if "bracelets" not in page.title.lower():
+        return
+    
+    markdown += "## Magic'Trilogy\n\n"
+
+    markdown += '<div class="grid cards" markdown>\n\n'
+
+    for file_path in sorted(glob.glob(f"{config.docs_dir}/assets/images/bracelets/magic_trilogy/*")):
+        f = open(file_path, 'rb')
+        tags = exifread.process_file(f)
+        description = tags.get("Image ImageDescription")
+        if description is None:
+            print(f"ERROR: {file_path} has no 'ImageDescription' ({tags})")
+
+        file_url = Path(file_path).relative_to(config.docs_dir)
+
+        markdown += f"- ![{description}]({file_url}){{lazyload}}\n    {description}\n"
+    
+    # context["images"] = images
+
+    markdown += '\n</div>\n'
+
+    print(markdown)
+    return markdown
